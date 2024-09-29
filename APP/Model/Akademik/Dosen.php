e03baa35afd9869423948536a57ee2c27c649778
Maidaalghazali <h1101231059@student.untan.ac.id>
2024-09-29 16:57:36 +0700

    Praktikum 6Dx - Implementasi Enkapsulasi dan Inheritance

--- APP/Model/Akademik/Dosen.php
+++ APP/Model/Akademik/Dosen.php
@@ -2,7 +2,7 @@
 namespace App\Admin;
 require_once 'App/Admin/Pegawai.php';
 class Dosen extends Pegawai {
-    public $nidn;
+    private $nidn;
     public function mengajar() : void {
         echo "{$this->nama} sedang mengajar perkuliahan";
     }
